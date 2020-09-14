76122464bff53f5bcb52574cdafa352c7a9bd970
Michal Vlasák <lahcim8@gmail.com>
2020-09-14 18:09:45 +0200

    Improve all aspects.
    Reworked package building process from `PKGBUILD` as a primary source of
    information + `build-deb` sourcing it, to `package-builder`.
    
    `package-builder` downloads are sources listed in `sources` file, and
    instructions in `build` to build package to a directory. From this
    directory either Debian, Arch Linux or tar package can be created.
    
    The biggest step is building `luatex` manually. This additionally requires
    bootstrapping `ctangle` as that is required for `mplib`.

---
+++ include/ff-config.h
@@ -0,0 +1,20 @@
+#ifndef FF_CONFIG_H
+#define FF_CONFIG_H
+
+#define _NO_FFSCRIPT 1
+
+/* All of this is standard. */
+#define HAVE_INTTYPES_H 1
+#define HAVE_LANGINFO_H 1
+#define HAVE_LONG_DOUBLE 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define STDC_HEADERS 1
+#define _HAS_LONGLONG 1
+
+#endif /* !FF_CONFIG_H */
---
+++ include/kpathsea/c-auto.h
@@ -0,0 +1,58 @@
+#ifndef KPATHSEA_C_AUTO_H
+#define KPATHSEA_C_AUTO_H
+
+#define KPSEVERSION "kpathsea version 6.3.2"
+#define KPSE_PACKAGE "kpathsea"
+#define KPSE_PACKAGE_BUGREPORT "tex-k@tug.org"
+#define KPSE_PACKAGE_NAME "Kpathsea"
+#define KPSE_PACKAGE_STRING "Kpathsea 6.3.2"
+#define KPSE_PACKAGE_TARNAME "kpathsea"
+#define KPSE_PACKAGE_URL ""
+#define KPSE_PACKAGE_VERSION "6.3.2"
+#define KPSE_VERSION "6.3.2"
+
+/* Most of this should be standard. */
+#define HAVE_ASSERT_H 1
+#define HAVE_DECL_ISASCII 1
+#define HAVE_DECL_PUTENV 1
+#define HAVE_DIRENT_H 1
+#define HAVE_DLFCN_H 1
+#define HAVE_FLOAT_H 1
+#define HAVE_FSEEKO 1
+#define HAVE_GETCWD 1
+#define HAVE_GETWD 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_LIMITS_H 1
+#define HAVE_MEMCMP 1
+#define HAVE_MEMCPY 1
+#define HAVE_MEMORY_H 1
+#define HAVE_MKSTEMP 1
+#define HAVE_MKTEMP 1
+#define HAVE_PUTENV 1
+#define HAVE_PWD_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRCHR 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_STRRCHR 1
+#define HAVE_STRUCT_STAT_ST_MTIM 1
+#define HAVE_SYS_PARAM_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define STDC_HEADERS 1
+
+/* 64-bit only for now. */
+#define SIZEOF_LONG 8
+
+/* Don't make anything, please. */
+#define MAKE_OMEGA_OCP_BY_DEFAULT 0
+#define MAKE_OMEGA_OFM_BY_DEFAULT 0
+#define MAKE_TEX_FMT_BY_DEFAULT 0
+#define MAKE_TEX_MF_BY_DEFAULT 0
+#define MAKE_TEX_PK_BY_DEFAULT 0
+#define MAKE_TEX_TEX_BY_DEFAULT 0
+#define MAKE_TEX_TFM_BY_DEFAULT 0
+
+#endif /* !KPATHSEA_C_AUTO_H */
